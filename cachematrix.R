549daba9e43e714dd5c16855b35d24e6af1a1f0e
setjmp: Eric Allen Griff <setjmp@hotmail.com>
2015-04-26 17:21:49 -0400

    Fixed Comment that wasn't commented :)

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Assignment: Caching the Inverse of a Matrix
-Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
